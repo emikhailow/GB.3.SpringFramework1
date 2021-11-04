dad62f3afa2f877968e9964dfe01b9de2282600d
Evgenii Mikhailov <eugene.mikhailow@gmail.com>
2021-11-04 18:01:11 +0300

    Перенес описание сервлета в аннотацию

--- src/main/java/com/geekbrains/homework1/ProductsServlet.java
+++ src/main/java/com/geekbrains/homework1/ProductsServlet.java
@@ -1,14 +1,15 @@
 package com.geekbrains.homework1;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(name = "ProductsServlet", urlPatterns = "/products")
 public class ProductsServlet extends HttpServlet {
-
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
